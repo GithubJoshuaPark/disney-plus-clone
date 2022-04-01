38a418b8f136944b18cad9497e399e49d1859009
JoshuaPark <soromiso@gmail.com>
2022-04-02 06:02:59 +0900

    the first commit

---
+++ readmeForThis.md
@@ -0,0 +1,28 @@
+### Exercising ReactJS building desney-plus web site using
+[ref ReactJS Tutorial for Beginners - Full Course in 20 Hours [2022]](https://www.youtube.com/watch?v=0mVbNp1ol_w&list=PLxF42TszRi96AOvErXMKqKe-oPdKxsT53&index=4)
+[ref resources](https://drive.google.com/drive/folders/1G5RYGvHSj7eXL5m6f7GoyF35a6vxUR1J)
+> (Within the footage: 2:24 ~ )
+
+> ReactJs project 생성
+```
+    $npx create-react-app disney-plus-clone --template redux
+    $cd disney-plus-clone
+```
+
+> Using firebase auth (login)
+
+> materail ui icons 사용위해 npm install
+> [ref https://v4.mui.com/components/material-icons/](https://v4.mui.com/components/material-icons/)
+
+```
+    $npm install --save react@^16.14.0 react-dom@16.14.0 // react version 16으로 낮추기
+    $npm install styled-components
+    $npm install @material-ui/core
+    $npm install @material-ui/icons  // material ui icon install 
+```
+> React Reveal (animation library) install
+> [ref https://www.react-reveal.com/](https://www.react-reveal.com/)
+```
+    $npm i react-reveal
+```
+> gitHub push 시 파일 하나 당 100M size 제한 있음
---
+++ src/components/Header.js
@@ -0,0 +1,109 @@
+import React from "react";
+import styled from "styled-components";
+
+function Header() {
+  return (
+    <Nav>
+      <Logo src="/images/logo.svg" />
+      <NavMenu>
+        <a>
+          <img src="/images/home-icon.svg" />
+          <span>HOME</span>
+        </a>
+        <a>
+          <img src="/images/search-icon.svg" />
+          <span>SEARCH</span>
+        </a>
+        <a>
+          <img src="/images/watchlist-icon.svg" />
+          <span>WATCHLIST</span>
+        </a>
+        <a>
+          <img src="/images/original-icon.svg" />
+          <span>ORIGINAL</span>
+        </a>
+        <a>
+          <img src="/images/movie-icon.svg" />
+          <span>MOVIES</span>
+        </a>
+        <a>
+          <img src="/images/series-icon.svg" />
+          <span>SERIES</span>
+        </a>
+      </NavMenu>
+      <UserImg src="/images/joshua.png" />
+    </Nav>
+  );
+}
+
+export default Header;
+
+const Nav = styled.nav`
+  height: 70px;
+  background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+  @media (max-width: 860px) {
+    justify-content: space-between;
+  }
+`;
+
+const Logo = styled.img`
+  width: 80px;
+  cursor: pointer;
+`;
+
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 25px;
+
+  @media (max-width: 860px) {
+    display: none;
+  }
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+
+    img {
+      height: 20px;
+    }
+
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      position: relative;
+
+      &:after {
+        content: "";
+        height: 2px;
+        background: white;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        opacity: 0;
+        transform-origin: left center;
+        transform: scaleX(0);
+      }
+    }
+
+    &:hover {
+      span:after {
+        transform: scaleX(1);
+        opacity: 1;
+        transition: 0.3s;
+      }
+    }
+  }
+`;
+
+const UserImg = styled.img`
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  cursor: pointer;
+`;
