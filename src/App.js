255ef931294276a280090bc3e40e468fd3de984c
JoshuaPark <soromiso@gmail.com>
2022-04-25 09:52:27 +0900

    source reviewd

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Login from "./components/Login";
 import FirebaseFireStoreTest from './components/FirebaseFireStoreTest';
 import FirebaseAuthTest from './components/FirebaseAuthTest';
 
-// eact-router-dom이 버전 6로 업그레이드되면서, Switch를 더이상 지원을 안하게 됬다
+// react-router-dom이 버전 6로 업그레이드되면서, Switch를 더이상 지원을 안하게 됬다
 // Switch -> routes로 바뀜. 또한 component도 element로 바꼈다.
 function App() {
   return (
